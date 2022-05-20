2fc6028a40992194bb2908bbffbcaf7c6163f52e
Lucas Borges <diasb.trabalho@gmail.com>
2022-05-20 16:20:55 -0300

    Traduzindo as views

--- spec/system/shipping_company/admin_view_shipping_company_page_spec.rb
+++ spec/system/shipping_company/admin_view_shipping_company_page_spec.rb
@@ -2,10 +2,17 @@ require "rails_helper"
 
 describe "Administrador acessa a página de transportadoras" do
 	it "com sucesso" do
+		Admin.create!(email:"lucas@staynight.com",password:"password",name:"Lucas")
 		ShippingCompany.create!(brand_name: "Quicksilver LTDA",corporate_name:"Quicksilver",registration_number:"12345678910110",email_domain: "quick.com",billing_address:"Carlos Reis, 152 RJ")
 
 		visit root_path
 		click_on "Entrar como Administrador"
+		fill_in "E-mail", with:"lucas@staynight.com"
+		fill_in "Senha", with: "password"
+		within "form" do
+			click_on "Entrar"
+		end
+		click_on "Transportadoras"
 
 		expect(page).to have_content "Transportadoras Filiadas"
 		expect(page).to have_content "Quicksilver"
@@ -13,10 +20,21 @@ describe "Administrador acessa a página de transportadoras" do
 	end
 
 	it "sem transportadoras cadastradas" do
+		Admin.create!(email:"lucas@staynight.com",password:"password",name:"Lucas")
+
 		visit root_path
 		click_on "Entrar como Administrador"
+		fill_in "E-mail", with:"lucas@staynight.com"
+		fill_in "Senha", with: "password"
+		within "form" do
+			click_on "Entrar"
+		end
+		click_on "Transportadoras"
 
 		expect(page).to have_content "Transportadoras Filiadas"
 		expect(page).to have_content "Nenhuma transportadora cadastrada"
 	end 
+
+	it "criar conta de Administrador" do
+	end
 end
