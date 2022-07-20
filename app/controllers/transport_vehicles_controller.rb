faa66d37734c582dda0413702e98a25003840e11
Lucas Borges <diasb.trabalho@gmail.com>
2022-07-20 20:52:43 -0300

    refactor: Refatoração de controllers

--- app/controllers/transport_vehicles_controller.rb
+++ app/controllers/transport_vehicles_controller.rb
@@ -4,12 +4,9 @@ class TransportVehiclesController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
   def show
-    @transport_vehicles = []
     @endereco = ShippingCompany.find(params[:id])
     if admin_signed_in? || (user_signed_in? && current_user.shipping_company_id == @endereco.id)
-      TransportVehicle.where(shipping_company_id: params[:id]).find_each do |tv|
-        @transport_vehicles << tv
-      end
+      @transport_vehicles = TransportVehicle.where(shipping_company_id: params[:id])
     else
       redirect_to root_path, notice: 'Página disponível apenas para Administradores ou Usuários desta Transportadora'
     end
@@ -29,8 +26,14 @@ class TransportVehiclesController < ApplicationController
     if @transport_vehicle.save
       redirect_to transport_vehicle_path(current_user.id), notice: 'Veículo cadastrado com sucesso!'
     else
-      flash.now[:notice] = 'Falha ao cadastrar o veículo'
-      render :new
+      rendering_failure
     end
+  end
+
+  private
+
+  def rendering_failure
+    flash.now[:notice] = 'Falha ao cadastrar o veículo'
+    render :new
   end
 end
